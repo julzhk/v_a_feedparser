780009807ec5dda1cbc39def22c7d57fc0e59d7a
Julian Harley <julz@Julians-MacBook-Pro.local>
2013-03-29 14:24:45 +0000

    populate model with api data

--- museum_object_manager/models.py
+++ museum_object_manager/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from museum_object_manager.museum_api import full_record_details
 
 class MuseumRecord(models.Model):
     api_id = models.IntegerField(null=True)
-    raw_data = models.TextField(null=True)
+    raw_data = models.TextField(blank=True)
 
     def __unicode__(self):
         return 'museum object:%s' % (self.api_id)
 
+    def save(self):
+        self.raw_data = full_record_details(id=self.api_id)
+        super(MuseumRecord, self).save()
