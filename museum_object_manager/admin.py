288087414b6c6a1376a67a6856a90891ff47bcbb
Julian Harley <julz@Julians-MacBook-Pro.local>
2013-03-29 15:53:06 +0000

    add api image preview to admin

--- museum_object_manager/admin.py
+++ museum_object_manager/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from museum_object_manager.models import MuseumRecord
+from django.utils.safestring import mark_safe
 
-admin.site.register(MuseumRecord)
+class MuseumRecordAdmin(admin.ModelAdmin):
+    exclude = ('raw_data',)
+    readonly_fields = ('img_preview',)
+
+    def img_preview(self, instance):
+        return '<img src="%s">' % instance.get_primary_image
+    img_preview.short_description = "Api Img"
+    img_preview.allow_tags = True
+
+admin.site.register(MuseumRecord,MuseumRecordAdmin)
